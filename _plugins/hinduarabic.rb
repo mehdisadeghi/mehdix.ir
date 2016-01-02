44bfa3d7479aec8e1dbb6306a3f14e4a7df07661
Mehdi Sadeghi <sadeghi@mehdix.org>
2016-01-02 17:14:24 +0100

    Convert inputs to string

--- _plugins/hinduarabic.rb
+++ _plugins/hinduarabic.rb
@@ -5,6 +5,7 @@ module Jekyll
   module HinduArabicFilter
 
     def habify(input)
+      input = input.to_s
       source = {"1" => "۱", "2" => "۲", "3" => "۳", "4" => "۴", "5" => "۵", "6" => "۶", "7" => "۷", "8" => "۸", "9" => "۹", "0" => "۰"}
       for x in source.keys
         if input.include? x
