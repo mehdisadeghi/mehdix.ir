fe7e03135372e30da13aa5c5df8734ee6e785e61
Mehdi Sadeghi <mehdi@mehdix.org>
2017-12-24 23:59:43 +0100

    Fix branch name.

--- push.sh
+++ push.sh
@@ -6,5 +6,5 @@ git config --global user.name "Travis CI"
 git add $COMMENT_DIR
 git commit --message "Travis static comment build: $TRAVIS_BUILD_NUMBER"
 
-git remote add origin-pages https://${GH_TOKEN}@github.com/$GH_USERNAME/$GH_REPO.git > /dev/null 2>&1
-git push --quiet --set-upstream origin master
+git remote add origin https://${GH_TOKEN}@github.com/$GH_USERNAME/$GH_REPO.git
+git push --set-upstream origin master
