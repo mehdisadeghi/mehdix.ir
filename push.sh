5e4ce96cec4aa71a53277e8188168eed01ccfe35
Mehdi Sadeghi <mehdi@mehdix.org>
2017-12-25 00:44:17 +0100

    Make push executable.

--- push.sh
+++ push.sh
@@ -3,8 +3,12 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
+echo PWD: `pwd`
+echo Git branch: `git branch`
+echo Git adding: $COMMENT_DIR
+git checkout master
 git add $COMMENT_DIR
 git commit --message "Travis static comment build: $TRAVIS_BUILD_NUMBER"
 
 git remote add origin https://${GH_TOKEN}@github.com/$GH_USERNAME/$GH_REPO.git
-git push --set-upstream origin master
+git push --quiet --set-upstream origin master
