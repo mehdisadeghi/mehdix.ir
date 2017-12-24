fde173e0c96c9482892db3b63cc41f230a51e1fb
Mehdi Sadeghi <mehdi@mehdix.org>
2017-12-24 22:59:51 +0100

    Add static comment build system.

---
+++ push.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+git add _data
+git commit --message "Travis static comment build: $TRAVIS_BUILD_NUMBER"
+
+git remote add origin-pages https://${GH_TOKEN}@github.com/$GH_USERNAME/$GH_REPO.git > /dev/null 2>&1
+git push --quiet --set-upstream origin master
