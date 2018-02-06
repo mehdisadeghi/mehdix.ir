0883909e52b3eeb51506322b2a1d0bbbf9ab4326
Mehdi Sadeghi <msk1361@gmail.com>
2018-02-06 02:43:11 +0100

    Disable git push.

--- build.sh
+++ build.sh
@@ -4,23 +4,24 @@
 # Exit on any error
 set -e
 
-echo "Install python deps for building comments."
-pip install requests pyyaml cryptography
-
 echo "Set git credentials."
 git config --global user.email "msk1361@gmail.com"
 git config --global user.name "Mehdi Sadeghi"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
-git checkout $BRANCH
-git pull origin $BRANCH
+echo "Install python deps for building comments."
+pip install requests pyyaml cryptography
 
 echo "Build static comments if there are any."
 python rebuild_comments.py
 
 # Disable exiting on errors automatically
 set +e
+
+#git checkout $BRANCH
+#git pull origin $BRANCH
+
 git add ./_source/_data/comments
 git commit --message "Netlify - Update static comments." > /dev/null
 
