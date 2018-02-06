a2a28494ffc0e711453f6d1b5c3da28548341298
Mehdi Sadeghi <msk1361@gmail.com>
2018-02-06 01:13:37 +0100

    Fix commiting back comments.

--- build.sh
+++ build.sh
@@ -7,30 +7,28 @@ set -e
 echo "Install python deps for building comments."
 pip install requests pyyaml cryptography
 
-echo "Build static comments if there are any."
-python rebuild_comments.py
-
 echo "Set git credentials."
 git config --global user.email "msk1361@gmail.com"
 git config --global user.name "Mehdi Sadeghi"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
+git checkout $BRANCH
+git pull origin $BRANCH
+
+echo "Build static comments if there are any."
+python rebuild_comments.py
 
 # Disable exiting on errors automatically
 set +e
-
 git add ./_source/_data/comments
 git commit --message "Netlify - Update static comments." > /dev/null
 
 if [ $? -eq 0 ]; then
-  git remote -v
-  git status
-	echo "Pushing new comments."
-	git push origin HEAD:master
-else
-  echo "Something went wrong while committing"
-  git status
+  echo "Pushing new comments."
+  git remote remove origin
+  git remote add origin https://github.com/mehdisadeghi/mehdix.ir
+  git push origin $BRANCH
 fi
 
 echo "Building the website."
