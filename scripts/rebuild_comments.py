6225091473919b07d4831ca68cc2f6d4e372fcbb
Mehdi Sadeghi <msk1361@gmail.com>
2020-04-08 09:59:43 +0200

    Update theme to 4.0.1

--- scripts/rebuild_comments.py
+++ scripts/rebuild_comments.py
@@ -1,15 +1,14 @@
 """Netlify comments processing script."""
-import os
-import json
 import hashlib
+import json
+import os
 from collections import OrderedDict
 
-import yaml
 import requests
-from pathlib2 import Path
+import yaml
 from cryptography.fernet import Fernet
 from langdetect import DetectorFactory, detect
-
+from pathlib2 import Path
 
 # Stay consistent between builds
 DetectorFactory.seed = 0
@@ -23,15 +22,16 @@ SITE_ID = os.environ["NETLIFY_SITE_ID"]
 
 BASE = "https://api.netlify.com/api/v1"
 SITE = "{base}/sites/{site_id}".format(base=BASE, site_id=SITE_ID)
-SUBMISSIONS_ENDPOINT = "{site}/forms/{form_id}/submissions?access_token={access_token}".format(
-    site=SITE, form_id=FORM_ID, access_token=ACCESS_TOKEN
-)
+SUBMISSIONS_ENDPOINT = \
+    "{site}/forms/{form_id}/submissions?access_token={access_token}".format(
+        site=SITE, form_id=FORM_ID, access_token=ACCESS_TOKEN)
 
 
 def get_comments():
     """Get a map of post_uuid => list of comment dicts."""
     raw_comments = requests.get(
-        "{site}/forms/{form_id}/submissions".format(site=SITE, form_id=FORM_ID),
+        "{site}/forms/{form_id}/submissions".format(
+            site=SITE, form_id=FORM_ID),
         params={"access_token": ACCESS_TOKEN},
     )
     comments = json.loads(raw_comments.content.decode("utf-8"))
@@ -55,7 +55,8 @@ def transform_comment(comment):
         "reply_to": comment["data"].get("reply-to"),
         "page_id": comment["data"]["page_id"],
         "name": comment["data"]["name"],
-        "email": hashlib.md5(comment["data"]["email"].encode("ascii")).hexdigest(),
+        "email": hashlib.md5(comment["data"]["email"].encode(
+            "ascii")).hexdigest(),
         "bucket": encrypt(comment["data"]["email"]),
         "website": comment["data"]["website"],
         "message": comment["data"]["message"],
@@ -78,24 +79,26 @@ def decrypt(text):
     return json.loads(s)
 
 
-def update_comments(file, comments):
+def update_comments(file, comments, page_id):
     """Update comments in the YAML data files of each post."""
     file.seek(0)
     old_comments = yaml.safe_load(file) or []
 
     # Use comment date as ID
     old_comment_ids = [cmnt["created_at"] for cmnt in old_comments]
-    # Avoid duplicates and avoid non-Persian comments. A naive protection against spam.
+    # Avoid duplicates and avoid non-Persian comments.
+    # A naive protection against spam.
     new_comments = list(
         filter(
-            lambda x: x["created_at"] not in old_comment_ids
-            ,#and x["language"] in ("fa", "ar"),
+            lambda x: x["created_at"]
+            not in old_comment_ids,  # and x["language"] in ("fa", "ar"),
             map(transform_comment, comments),
         )
     )
 
     if new_comments:
-        yaml.dump(new_comments, file, default_flow_style=False, allow_unicode=True)
+        yaml.dump(
+            new_comments, file, default_flow_style=False, allow_unicode=True)
 
 
 def main():
@@ -103,11 +106,12 @@ def main():
     netlify_comments = get_comments()
     Path(COMMENT_DIR).mkdir(parents=True, exist_ok=True)
     for page_uuid, page_comments in netlify_comments.items():
+        print('Building', page_uuid)
         if page_comments:
             uid = os.path.basename(page_comments[0]["data"]["page_id"])
             path = os.path.join(COMMENT_DIR, "{uid}.yml".format(uid=uid))
             with open(path, "a+", encoding="utf8") as file:
-                update_comments(file, page_comments)
+                update_comments(file, page_comments, page_uuid)
 
 
 if __name__ == "__main__":
