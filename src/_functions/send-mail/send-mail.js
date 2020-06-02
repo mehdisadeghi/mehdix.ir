0ee0ded8defcec48b43c03ed7d9a42d874d33b91
Mehdi Sadeghi <msk1361@gmail.com>
2020-06-02 11:13:30 +0200

    Drop mailgun.

--- src/_functions/send-mail/send-mail.js
+++ src/_functions/send-mail/send-mail.js
@@ -27,11 +27,11 @@ async function main(event, context){
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: 'smtp.mailgun.org',
+    host: process.env.SMTP_HOST,
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
-      user: process.env.SMTP_HOST,
+      user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS
     }
   });
