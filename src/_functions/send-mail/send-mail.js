b1d0f194b4a19d860bf69942d6dbe09bd95265fd
Mehdi Sadeghi <msk1361@gmail.com>
2020-06-05 14:04:40 +0200

    Notify author and OP.

--- src/_functions/send-mail/send-mail.js
+++ src/_functions/send-mail/send-mail.js
@@ -1,29 +1,67 @@
-// Mail OP upon reply.
+/* 
+This Function will notify the article author and/or a comment OP about a new comment.
+If the comment is a reply to a previous comment (submission), the OP will be notified.
+If not, only the author of the post will be informed.
+TODO:
+- Add subscribe/unsubscribe links
+- Email only if the user has a subscription
+- Find the post author, if there is not one, use the site author's email
+- Make sure there is a valid user-authentication-token in the submission
+- Only send mail to validated email addresses
+*/
 "use strict";
 const nodemailer = require('nodemailer');
 const request = require('request-promise-native');
 
-exports.handler = async function(event, context) {
-  try {
-    const body = await main(event, context);
-    return { statusCode: 200, body };
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() };
-  }
+
+// module.exports.hello = async event => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(
+//       {
+//         message: 'Go Serverless v1.0! Your function executed successfully!',
+//         input: event,
+//       },
+//       null,
+//       2
+//     ),
+//   };
+
+//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+// };
+
+
+exports.handler = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: await main(JSON.parse(event.body)),
+        input: event,
+      }
+    ),
+    null,
+    2
+  };
 };
 
 // async..await is not allowed in global scope, must use a wrapper
-async function main(event, context){
-  console.log('Event:', event);
-  let body = JSON.parse(event.body);
-  console.log('After parse:', body);
+async function main(submission) {
 
-  if(!body.data['reply-to']){
-    return Promise.resolve('Not a reply.');
-  }
+  console.log(submission);
 
-  let opEmail = await getOPEmail(body.data['reply-to'])
-  console.log('OP Email:', opEmail);
+  // The link back to the submission
+  let submissionURL = submission.site_url + submission.data.page_id + ".html#" + submission.id;
+
+  // Construct the mail body for the OP
+  let mailTextBody = `${submission.name} به دیدگاهت رو سایت مهدیکس [جواب](${submissionURL}) داد:
+
+  ${submission.body}`;
+
+  let mailHTMLBody = `<div dir="rtl">
+  <p>${submission.name} به دیدگاهت رو سایت مهدیکس <a href="${submissionURL}">جواب</a> داد:</p>
+  <blockquote>${submission.body}</blockquoe></div>`;
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
@@ -36,29 +74,33 @@ async function main(event, context){
     }
   });
 
-  let replyLink = body.site_url + body.data.page_id + ".html#" + body.id;
+  // Notify OP if this is a reply to a previous comment
+  if (submission.data['reply-to']) {
+    // Get OP's email address
+    let opEmail = await getOPEmail(submission.data['reply-to'])
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: `mehdix.ir <${submission.id}@mehdix.ir>`,
+      to: opEmail,
+      subject: `${submission.name} جواب داد ✔`,
+      text: mailTextBody,
+      html: mailHTMLBody
+    });
+  }
 
-  let txt = `${body.name} به دیدگاهت رو سایت مهدیکس [جواب](${replyLink}) داد:
-
-    ${body.body}`;
-
-  let html = `<div dir="rtl">
-    <p>${body.name} به دیدگاهت رو سایت مهدیکس <a href="${replyLink}">جواب</a> داد:</p>
-    <blockquote>${body.body}</blockquoe></div>`;
-
-  // send mail with defined transport object
+  // Alwas inform the post author about new comments.
   let info = await transporter.sendMail({
-    from: `mehdix.ir <${body.id}@mehdix.ir>`,
-    to: opEmail,
-    subject: `${body.name} جواب داد ✔`,
-    text: txt,
-    html: html
+    from: `mehdix.ir <${submission.id}@mehdix.ir>`,
+    to: process.env.NOTIFICATIONS_EMAIL,
+    subject: `${submission.name} نظر داد ✔`,
+    text: mailTextBody,
+    html: mailHTMLBody
   });
 
   return Promise.resolve(`Message sent: ${info.messageId}`);
 }
 
-async function getOPEmail(replyToId){
+async function getOPEmail(replyToId) {
   let options = {
     uri: (
       `https://api.netlify.com/api/v1/sites/${process.env.NETLIFY_SITE_ID}` +
@@ -71,11 +113,11 @@ async function getOPEmail(replyToId){
 
   return Promise.resolve(
     request(options)
-      .then((submission)=>{
-        return submission.email
-      })
-      .catch((err)=>{
-        console.log(err)
-      })
-    );
+    .then((submission) => {
+      return submission.email
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  );
 }
